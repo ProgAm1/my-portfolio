9ae1f2be8ac5c320cf20d8e7cfbd4fb7dcee6f30
Prog-Ammar0 <2342950@uj.edu.sa>
2026-03-24 14:05:33 +0300

    fix skills background

--- components/skills-background.tsx
+++ components/skills-background.tsx
@@ -19,11 +19,11 @@ export default function SkillsBackground() {
     <div ref={ref} className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
       <motion.div
         style={{ y: y1 }}
-        className="absolute left-0 top-1/2 -translate-y-1/2 h-[300px] w-[300px] md:h-[500px] md:w-[500px] rounded-full bg-violet-700/[0.08] blur-[80px] md:blur-[120px]"
+        className="absolute -left-[100px] top-[10%] md:-left-[150px] md:top-[15%] h-[500px] w-[500px] md:h-[800px] md:w-[800px] rounded-full bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-violet-700/15 via-violet-700/5 to-transparent"
       />
       <motion.div
         style={{ y: y2 }}
-        className="absolute right-0 bottom-0 h-[200px] w-[250px] md:h-[300px] md:w-[400px] rounded-full bg-violet-700/[0.08] blur-[60px] md:blur-[100px]"
+        className="absolute -right-[50px] bottom-[-50px] md:-right-[100px] md:bottom-[-100px] h-[400px] w-[450px] md:h-[600px] md:w-[700px] rounded-[100%] bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-violet-700/15 via-violet-700/5 to-transparent"
       />
     </div>
   );
